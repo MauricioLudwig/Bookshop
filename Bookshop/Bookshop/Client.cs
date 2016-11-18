dd16c76ae9b88c23c7e1bbf5ea36fd149df970b3
MauricioLudwig <mrdamiengallagher@gmail.com>
2016-11-18 11:09:52 +0100

    Initial commit

---
+++ Bookshop/Bookshop/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshop
+{
+    class Client
+    {
+        public void Start()
+        {
+
+        }
+    }
+}
